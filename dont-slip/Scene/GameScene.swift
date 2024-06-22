f07ac726984dafb71e06ea10771beff8d6f74c03
Ariq Syahalam <70618885+ariqsyahalam@users.noreply.github.com>
2024-06-23 00:33:29 +0700

    leaderboard done

--- dont-slip/Scene/GameScene.swift
+++ dont-slip/Scene/GameScene.swift
@@ -1,4 +1,5 @@
 import SpriteKit
+import GameKit
 import GameplayKit
 
 enum CollisionMask: UInt32 {
@@ -10,30 +11,30 @@ enum CollisionMask: UInt32 {
 }
 
 class GameScene: SKScene, SKPhysicsContactDelegate {
-
+    
     /*
      USED
      */
-
+    
     // Entities
     var entityManager: EntityManager!
     var penguinEntity: Penguin?
     var penguinControls: PlayerControlComponent?
     var icebergEntity: Iceberg?
-
+    
     // Time-stuff
     var spawnInterval: TimeInterval = 2.0
     var obstacleSpawnInterval: TimeInterval = 2.0 // 2 - 0.5
     var coinSpawnInterval: TimeInterval = 2.0
-
+    
     var lastSpawnTime: TimeInterval = 0
     private var lastUpdateTime: TimeInterval = 0
-
+    
     // Constants
     private let restoringTorqueMult: CGFloat = 10.0
     private let dampingTorqueMult: CGFloat = 0.5
     private var gravMult: CGFloat = 1
-
+    
     // Might change in the middle of the game
     var icebergWidth = UIScreen.main.bounds.width * 0.8
     var penguinMass: CGFloat = 0.04
@@ -41,40 +42,40 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
     var accelSensitivity: CGFloat = 300
     var icebergFrictionLevel: Double = 0.0 // 0  - 0.5
     var obstacleMassMultiplier: Double = 1.0 // 1 - 2
-
+    
     // States (booleans)
     var isPenguinOnGround = false // Flag to track if the green cube is on the ground
     var gameOver: Bool = false
-
+    
     // Sound/Music
-
+    
     // Coins
     var coinsLabel: SKLabelNode!
     var coinsCollected: Int = 0
     var currentActiveCoins: Int = 0
-
+    
     // Points
     internal var pointsLabel: SKLabelNode! // Label to display points
     internal var points: Int = 0
-
+    
     // Accelerometer
     private var accelerometerManager: AccelerometerManager?
-
+    
     /*
      NEED TO EDIT
      */
-
+    
     // Scoring
     var score: Int = 0
     var highScore: Int = 0
     var scoreLabel: SKLabelNode!
     var highScoreLabel: SKLabelNode!
-
+    
     var totalCoins: Int = 0
     var totalCoinsLabel: SKLabelNode!
-
+    
     var greenCube: SKSpriteNode? // the cube (penguin)
-
+    
     // Modality game over
     var modalBackground: SKSpriteNode!
     var bigContainer: SKSpriteNode!
@@ -87,7 +88,7 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
     var totalCoinContainer: SKSpriteNode!
     
     var blueModalBackground: SKSpriteNode!
-        
+    
     var gameOverScoreLabel: SKLabelNode!
     var gameOverCoinsLabel: SKLabelNode!
     var gameOverLabel: SKSpriteNode!
@@ -101,7 +102,7 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
     var totalCoinsGameOverLabel: SKLabelNode!
     var coinImgOver: SKSpriteNode!
     var coinImgOver2: SKSpriteNode!
-
+    
     override func sceneDidLoad() {
         // Setup physicsWorld bases
         self.physicsWorld.contactDelegate = self
@@ -109,7 +110,7 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         addBackgroundImage()
         // Setup accelerometer
         self.accelerometerManager = AccelerometerManager(sensitivity: accelSensitivity)
-
+        
         loadHighScore() // Load high score
         loadCoins()
         setupEntities()
@@ -119,19 +120,19 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         createEndlessWaves() // Create the wave animations
         difficultyUpdater()
     }
-
+    
     // MARK: Setting up entities
     func setupEntities() {
         // Define EntityManager
         entityManager = EntityManager(scene: self)
-
+        
         /* Iceberg setup */
         // Define the iceberg entity
         let iceberg = Iceberg(imageName: "iceberg", entityManager: entityManager)
         // Add with entity manager
         entityManager.add(iceberg)
         icebergEntity = iceberg
-
+        
         /* Penguin: Pinjing Setup */
         let penguin = Penguin(
             imageName: "penguin",
@@ -139,37 +140,37 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
             groundNode: iceberg.component(ofType: SpriteComponent.self)?.node,
             mass: penguinMass
         )
-
+        
         print(penguin.component(ofType: SpriteComponent.self)?.node.physicsBody?.mass ?? "?")
-
+        
         if let spriteComponent = penguin.component(ofType: SpriteComponent.self) {
             self.greenCube = spriteComponent.node
         }
         entityManager.add(penguin)
         penguinEntity = penguin
-
+        
         penguinControls = getPenguinControls()
-
+        
         /* Obstacles Setup */
         // Randomly spawns OBSTACLES
         randomlySpawnObjects(spawnerFunction: spawnObstacle, interval: obstacleSpawnInterval)
-
+        
         /* Coins Setup */
         // Randomly spawns COINS
         randomlySpawnObjects(spawnerFunction: spawnCoin, interval: coinSpawnInterval)
     }
-
+    
     func getPenguinControls() -> PlayerControlComponent? {
         if let _ = penguinEntity?.component(ofType: SpriteComponent.self),
            let penguinControls = penguinEntity?.component(ofType: PlayerControlComponent.self) {
             print("getPenguinControls: got penguin controls :0")
             return penguinControls
         }
-
+        
         print("getPenguinControls: failed to get penguin controls!")
         return nil
     }
-
+    
     func checkGameOver() {
         guard let greenCube = greenCube else { return }
         if greenCube.position.y < -self.size.height / 2 { // Check if cube is below the visible screen
@@ -185,6 +186,7 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
             if score > highScore {
                 highScore = score
                 saveHighScore()
+                reportScoreToGameCenter(score: highScore) // Tambahkan ini untuk mengirim skor ke Game Center
             }
             // showRestartButton()
             showGameOverModal()
@@ -192,40 +194,53 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         }
     }
 
+    func reportScoreToGameCenter(score: Int) {
+        if GKLocalPlayer.local.isAuthenticated {
+            let leaderboardID = "ligapinguinc7ada" // Ganti dengan ID leaderboard Anda
+            GKLeaderboard.submitScore(score, context: 0, player: GKLocalPlayer.local, leaderboardIDs: [leaderboardID]) { error in
+                if let error = error {
+                    print("Error reporting score to Game Center: \(error.localizedDescription)")
+                } else {
+                    print("Score successfully reported to Game Center")
+                }
+            }
+        }
+    }
+    
     func restartGame() {
         self.removeAllChildren()
         self.removeAllActions()
-
+        
         score = 0
         gameOver = false
-
+        
         sceneDidLoad() // Reinitialize the scene setup
     }
-
+    
     override func update(_ currentTime: TimeInterval) {
         // Check game over state before running update logic
         if gameOver {
             return
         }
-
+        
         if self.lastUpdateTime == 0 {
             self.lastUpdateTime = currentTime
         }
-
+        
         let dt = currentTime - self.lastUpdateTime
-
+        
         self.lastUpdateTime = currentTime
-
+        
         // Update entities inside the entity manager
         entityManager.update(deltaTime: dt)
-
+        
         // Update accelerometer
         accelerometerManager?.startAccelerometerUpdates()
-
+        
         // Check if game over
         checkGameOver()
     }
-
+    
     func resetTimers() {
         spawnInterval = 2.0
         obstacleSpawnInterval = 2.0
--- dont-slip/Scene/StartScene.swift
+++ dont-slip/Scene/StartScene.swift
@@ -1,8 +1,16 @@
 import SpriteKit
+import GameKit
 import GameplayKit
 
-class StartScene: SKScene {
+class StartScene: SKScene, GKGameCenterControllerDelegate {
+    func gameCenterViewControllerDidFinish(_ gameCenterViewController: GKGameCenterViewController) {
+        gameCenterViewController.dismiss(animated: true, completion: nil)
+    }
+    
     override func didMove(to view: SKView) {
+        
+        authenticateUser()
+        
         // Set up background image
         let background = SKSpriteNode(imageNamed: "bg.png")
         background.position = CGPoint(x: self.frame.midX, y: self.frame.midY)
@@ -34,8 +42,35 @@ class StartScene: SKScene {
         startButton.zPosition = 3
         addChild(startButton)
         
+        // Set up leaderboard button
+         let leaderboardButton = SKSpriteNode(imageNamed: "StartButton.png")
+         leaderboardButton.position = CGPoint(x: self.frame.midX, y: self.frame.midY - 150)
+         leaderboardButton.name = "leaderboardButton"
+         leaderboardButton.zPosition = 5
+         addChild(leaderboardButton)
+        
         
     }
+    
+    func authenticateUser() {
+        let localPlayer = GKLocalPlayer.local
+        localPlayer.authenticateHandler = { (viewController, error) in
+            if let vc = viewController {
+                if let viewController = self.view?.window?.rootViewController {
+                    viewController.present(vc, animated: true, completion: nil)
+                }
+            } else if localPlayer.isAuthenticated {
+                print("User authenticated successfully")
+            } else {
+                if let error = error {
+                    print("Game Center authentication failed with error: \(error.localizedDescription)")
+                } else {
+                    print("Game Center authentication failed for unknown reason")
+                }
+            }
+        }
+    }
+    
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         for touch in touches {
@@ -67,102 +102,117 @@ class StartScene: SKScene {
                         
                     }
                 }
+            } else if node.name == "leaderboardButton" {
+                showLeaderboard()
             }
+
         }
     }
-            
-            func createEndlessWaves() {
-                let waveNode1 = SKShapeNode()
-                waveNode1.strokeColor = .clear
-                waveNode1.fillColor = .topwave
-                waveNode1.lineWidth = 2
-                waveNode1.zPosition = 1 // In front of iceberg
-                self.addChild(waveNode1)
-                
-                // Create the second wave node with light blue color
-                let waveNode2 = SKShapeNode()
-                waveNode2.strokeColor = .clear
-                waveNode2.fillColor = .midwave
-                waveNode2.lineWidth = 2
-                waveNode2.zPosition = 2 // In front of waveNode1
-                self.addChild(waveNode2)
-                
-                // Create the third wave node with magenta color
-                let waveNode3 = SKShapeNode()
-                waveNode3.strokeColor = .clear
-                waveNode3.fillColor = .basewave
-                waveNode3.lineWidth = 2
-                waveNode3.zPosition = 3 // In front of waveNode2
-                self.addChild(waveNode3)
-                
-                let waveWidth = self.size.width * 2
-                let wavePath = createWavePath(amplitude: 10, wavelength: 250, width: waveWidth)
-                
-                waveNode1.path = wavePath
-                waveNode2.path = wavePath
-                waveNode3.path = wavePath
-                
-                // Position the waves slightly below each other
-                waveNode1.position = CGPoint(x: 0, y: -50)
-                waveNode2.position = CGPoint(x: 0, y: -60)
-                waveNode3.position = CGPoint(x: 0, y: -70)
-                
-                let moveDuration1: TimeInterval = 5
-                let moveDuration2: TimeInterval = 10
-                let moveDuration3: TimeInterval = 15
-                
-                let moveLeft1 = SKAction.moveBy(x: -self.size.width, y: 0, duration: moveDuration1)
-                let moveReset1 = SKAction.moveBy(x: self.size.width, y: 0, duration: 0)
-                let moveSequence1 = SKAction.sequence([moveLeft1, moveReset1])
-                let moveForever1 = SKAction.repeatForever(moveSequence1)
-                
-                let moveLeft2 = SKAction.moveBy(x: -self.size.width, y: 0, duration: moveDuration2)
-                let moveReset2 = SKAction.moveBy(x: self.size.width, y: 0, duration: 0)
-                let moveSequence2 = SKAction.sequence([moveLeft2, moveReset2])
-                let moveForever2 = SKAction.repeatForever(moveSequence2)
-                
-                let moveLeft3 = SKAction.moveBy(x: -self.size.width, y: 0, duration: moveDuration3)
-                let moveReset3 = SKAction.moveBy(x: self.size.width, y: 0, duration: 0)
-                let moveSequence3 = SKAction.sequence([moveLeft3, moveReset3])
-                let moveForever3 = SKAction.repeatForever(moveSequence3)
-                
-                waveNode1.run(moveForever1)
-                waveNode2.run(moveForever2)
-                waveNode3.run(moveForever3)
-            }
-            
-            func createWavePath(amplitude: CGFloat, wavelength: CGFloat, width: CGFloat) -> CGPath {
-                let path = CGMutablePath()
-                let waveHeight: CGFloat = self.size.height / 2
-                
-                // Start at the bottom left corner
-                path.move(to: CGPoint(x: 0, y: 0))
-                
-                // Move up to the starting point of the wave
-                path.addLine(to: CGPoint(x: 0, y: waveHeight))
-                
-                // Draw the wave
-                for x in stride(from: 0, to: width, by: 5) { // Increased step size for better performance
-                    let y = amplitude * sin(x / wavelength * 2 * .pi) + waveHeight
-                    path.addLine(to: CGPoint(x: x, y: y))
-                }
-                
-                // Move down to the bottom right corner
-                path.addLine(to: CGPoint(x: width, y: 0))
-                
-                // Close the path to form a complete shape
-                path.closeSubpath()
-                
-                return path
-            }
-            
-            func startTilting(node: SKNode) {
-                let tiltRight = SKAction.rotate(byAngle: CGFloat.pi / 20, duration: 1)
-                let tiltLeft = SKAction.rotate(byAngle: -CGFloat.pi / 20, duration: 1)
-                let tiltBackToCenter = SKAction.rotate(toAngle: 0, duration: 1)
-                let tiltSequence = SKAction.sequence([tiltLeft, tiltBackToCenter, tiltRight, tiltBackToCenter])
-                let tiltForever = SKAction.repeatForever(tiltSequence)
-                node.run(tiltForever)
-            }
+    
+    func showLeaderboard() {
+        if GKLocalPlayer.local.isAuthenticated {
+            let viewController = self.view?.window?.rootViewController
+            let gcViewController = GKGameCenterViewController(leaderboardID: "your.leaderboard.id", playerScope: .global, timeScope: .allTime)
+            gcViewController.gameCenterDelegate = self
+            viewController?.present(gcViewController, animated: true, completion: nil)
+        } else {
+            print("User is not authenticated to access Game Center")
         }
+    }
+
+    
+    func createEndlessWaves() {
+        let waveNode1 = SKShapeNode()
+        waveNode1.strokeColor = .clear
+        waveNode1.fillColor = .topwave
+        waveNode1.lineWidth = 2
+        waveNode1.zPosition = 1 // In front of iceberg
+        self.addChild(waveNode1)
+        
+        // Create the second wave node with light blue color
+        let waveNode2 = SKShapeNode()
+        waveNode2.strokeColor = .clear
+        waveNode2.fillColor = .midwave
+        waveNode2.lineWidth = 2
+        waveNode2.zPosition = 2 // In front of waveNode1
+        self.addChild(waveNode2)
+        
+        // Create the third wave node with magenta color
+        let waveNode3 = SKShapeNode()
+        waveNode3.strokeColor = .clear
+        waveNode3.fillColor = .basewave
+        waveNode3.lineWidth = 2
+        waveNode3.zPosition = 3 // In front of waveNode2
+        self.addChild(waveNode3)
+        
+        let waveWidth = self.size.width * 2
+        let wavePath = createWavePath(amplitude: 10, wavelength: 250, width: waveWidth)
+        
+        waveNode1.path = wavePath
+        waveNode2.path = wavePath
+        waveNode3.path = wavePath
+        
+        // Position the waves slightly below each other
+        waveNode1.position = CGPoint(x: 0, y: -50)
+        waveNode2.position = CGPoint(x: 0, y: -60)
+        waveNode3.position = CGPoint(x: 0, y: -70)
+        
+        let moveDuration1: TimeInterval = 5
+        let moveDuration2: TimeInterval = 10
+        let moveDuration3: TimeInterval = 15
+        
+        let moveLeft1 = SKAction.moveBy(x: -self.size.width, y: 0, duration: moveDuration1)
+        let moveReset1 = SKAction.moveBy(x: self.size.width, y: 0, duration: 0)
+        let moveSequence1 = SKAction.sequence([moveLeft1, moveReset1])
+        let moveForever1 = SKAction.repeatForever(moveSequence1)
+        
+        let moveLeft2 = SKAction.moveBy(x: -self.size.width, y: 0, duration: moveDuration2)
+        let moveReset2 = SKAction.moveBy(x: self.size.width, y: 0, duration: 0)
+        let moveSequence2 = SKAction.sequence([moveLeft2, moveReset2])
+        let moveForever2 = SKAction.repeatForever(moveSequence2)
+        
+        let moveLeft3 = SKAction.moveBy(x: -self.size.width, y: 0, duration: moveDuration3)
+        let moveReset3 = SKAction.moveBy(x: self.size.width, y: 0, duration: 0)
+        let moveSequence3 = SKAction.sequence([moveLeft3, moveReset3])
+        let moveForever3 = SKAction.repeatForever(moveSequence3)
+        
+        waveNode1.run(moveForever1)
+        waveNode2.run(moveForever2)
+        waveNode3.run(moveForever3)
+    }
+    
+    func createWavePath(amplitude: CGFloat, wavelength: CGFloat, width: CGFloat) -> CGPath {
+        let path = CGMutablePath()
+        let waveHeight: CGFloat = self.size.height / 2
+        
+        // Start at the bottom left corner
+        path.move(to: CGPoint(x: 0, y: 0))
+        
+        // Move up to the starting point of the wave
+        path.addLine(to: CGPoint(x: 0, y: waveHeight))
+        
+        // Draw the wave
+        for x in stride(from: 0, to: width, by: 5) { // Increased step size for better performance
+            let y = amplitude * sin(x / wavelength * 2 * .pi) + waveHeight
+            path.addLine(to: CGPoint(x: x, y: y))
+        }
+        
+        // Move down to the bottom right corner
+        path.addLine(to: CGPoint(x: width, y: 0))
+        
+        // Close the path to form a complete shape
+        path.closeSubpath()
+        
+        return path
+    }
+    
+    func startTilting(node: SKNode) {
+        let tiltRight = SKAction.rotate(byAngle: CGFloat.pi / 20, duration: 1)
+        let tiltLeft = SKAction.rotate(byAngle: -CGFloat.pi / 20, duration: 1)
+        let tiltBackToCenter = SKAction.rotate(toAngle: 0, duration: 1)
+        let tiltSequence = SKAction.sequence([tiltLeft, tiltBackToCenter, tiltRight, tiltBackToCenter])
+        let tiltForever = SKAction.repeatForever(tiltSequence)
+        node.run(tiltForever)
+    }
+}
 
